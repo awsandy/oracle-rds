3da5e59b58a7a8dccb1aff8276f35c0b8fb8d45e
awsandy <awsandy@amazon.co.uk>
2021-02-06 17:13:08 +0000

    up

--- dbinstall-2.sh
+++ dbinstall-2.sh
@@ -1,9 +1,16 @@
 echo "dbinstall 2 start" >> ~/dbinstall.txt
 cd ~
+echo "Starting silent dbca" >> ~/dbinstall.txt
+$ORACLE_HOME/bin/dbca -silent -responseFile /software/oracle-rds/dbca_orcl-1.rcp
+echo "Finished silent dbca" >> ~/dbinstall.txt
+echo "unpacking swingbench" >> ~/dbinstall.txt
+cd ~
 unzip -qq /software/swingbenchlatest.zip
 cd swingbench/bin
-
-
+echo "swingbench oewizard" >> ~/dbinstall.txt
+./oewizard -dbap ea96!qIVBTND -nopart -u soe -p soe -cl -cs //oracle.local/orcl -ts soe_tbs -create -dba system
+# inflate data
+#./sbutil -u soe -p soe  -cs //lab/martin -soe parallel 12 -dup 4
 echo "listener" >> ~/dbinstall.txt
 #lsnrctl start
 echo "dbinstall 2 done" >> ~/dbinstall.txt
