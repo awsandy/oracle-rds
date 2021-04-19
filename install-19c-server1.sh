d339ff95f9a0d3fdd98856f4866c5d98bd02f8a2
awsandy <awsandy@amazon.co.uk>
2021-04-19 22:30:03 +0100

    up

--- install-19c-server1.sh
+++ install-19c-server1.sh
@@ -58,16 +58,22 @@ echo "======= X11"
 date 
 #yum install -y xorg-x11-server-Xorg xorg-x11-xauth xorg-x11-apps
 #yum install "@X Window System" xorg-x11-xauth xorg-x11-fonts-* xorg-x11-utils –y
-echo "======= epel rpm"
-rpm -U https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+#echo "======= epel rpm"
+#rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 #yum groupinstall -y "Xfce"
-echo "======= group install MATE"
-#yum groupinstall -q -y 'X Window System' 'GNOME'
-yum groupinstall -y "MATE Desktop"
-echo "PREFERRED=/usr/bin/mate-session" > /etc/sysconfig/desktop
+#echo "======= group install MATE"
+# causes failure
+#yum groupinstall -y "MATE Desktop"
+#echo "PREFERRED=/usr/bin/mate-session" > /etc/sysconfig/desktop
+echo "======= group install GNome"
+yum groupinstall -y "Server with GUI"
+#yum groupinstall -y 'X Window System' 'GNOME'
+
 systemctl set-default graphical.target
+systemctl default
 echo "======= xrdp" 
 date 
+rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-1.el7.nux.noarch.rpm
 yum  -y install xrdp tigervnc-server
 #yum -y install xrdp
 echo "======= start xrdp"
