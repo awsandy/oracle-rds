d0aea22cac389ff03b2ff5372ea4a0dc83996c1b
awsandy <awsandy@amazon.co.uk>
2021-04-26 20:37:30 +0100

    up

--- tf-client/oewizard-1.sh
+++ tf-client/oewizard-1.sh
@@ -6,9 +6,12 @@ cd ~
 export ORACLE_SID=orcl
 
 db1=$(aws rds describe-db-instances --region eu-west-2 --db-instance-identifier dwp-demo-ha-az2 --query "DBInstances[].Endpoint.Address" | jq -r .[])
-
+nmap $db1 -Pn -p 1521 | grep open
 echo "swingbench oewizard"
 date
+
+dbp=$(aws secretsmanager get-secret-value --region eu-west-2 --secret-id db-creds | jq -r .SecretString | jq -r .password)
+
 # drop -cl run in char mode 
 #./oewizard -dbap manager -u soe -p soe -cl -cs //localhost/plorcl -ts SOE -drop
 # 
@@ -18,7 +21,7 @@ date
 #
 # create
 cd ~/swingbench/bin
-./oewizard  -dbap $1 -u soe -p soe -cl -cs //$db1/orcl -ts SOE -scale 1 -create
+./oewizard  -dba admin -dbap $dbp -u soe -p soe -cl -cs //$db1/orcl -ts SOE -scale 1 -create -tc 8 -v
 if [ $? -ne 0 ]; then
     echo "ERROR:  oewizard non zero exit code "
 fi
