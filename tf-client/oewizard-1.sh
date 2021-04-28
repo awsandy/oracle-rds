f0f9d61f5d422da0debf687c3d0947b5fb336d87
awsandy <awsandy@amazon.co.uk>
2021-04-28 20:07:19 +0100

    up

--- tf-client/oewizard-1.sh
+++ tf-client/oewizard-1.sh
@@ -4,80 +4,47 @@ echo "oewizard 2 start"
 date
 cd ~
 export ORACLE_SID=orcl
-
+cd ~/swingbench/bin
+dbp=$(aws secretsmanager get-secret-value --region eu-west-2 --secret-id db-config | jq -r .SecretString | jq -r .password)
 db1=$(aws rds describe-db-instances --region eu-west-2 --db-instance-identifier dwp-demo-ha1-az2 --query "DBInstances[].Endpoint.Address" | jq -r .[])
+db2=$(aws rds describe-db-instances --region eu-west-2 --db-instance-identifier dwp-demo-ha2-az2 --query "DBInstances[].Endpoint.Address" | jq -r .[])
+db3=$(aws rds describe-db-instances --region eu-west-2 --db-instance-identifier dwp-demo-single --query "DBInstances[].Endpoint.Address" | jq -r .[])
+
 if [ $db1 != "" ];then 
     nmap $db1 -Pn -p 1521 | grep open
     echo "swingbench oewizard"
     date
-
-    dbp=$(aws secretsmanager get-secret-value --region eu-west-2 --secret-id db-creds | jq -r .SecretString | jq -r .password)
-
-    # drop -cl run in char mode 
-    #./oewizard -dbap manager -u soe -p soe -cl -cs //localhost/plorcl -ts SOE -drop
-    # 
-    #
-    # get db hostname
-    # aws rds 
-    #
-    # create
-    cd ~/swingbench/bin
-    ./oewizard  -dba admin -dbap $dbp -u soe -p soe -cl -cs //$db1/orcl -ts SOE -scale 32 -create -tc 8 -v 
+   
+    time ./oewizard  -dba admin -dbap $dbp -u soe -p soe -cl -cs //$db1/orcl -ts SOE -scale 32 -create -tc 8 -v 
     if [ $? -ne 0 ]; then
         echo "ERROR:  oewizard non zero exit code "
     fi
 fi
 
 
-
-
-
-db1=$(aws rds describe-db-instances --region eu-west-2 --db-instance-identifier dwp-demo-ha2-az2 --query "DBInstances[].Endpoint.Address" | jq -r .[])
-if [ $db1 != "" ];then 
-    nmap $db1 -Pn -p 1521 | grep open
+if [ $db2 != "" ];then 
+    nmap $db2 -Pn -p 1521 | grep open
     echo "swingbench oewizard"
     date
 
-    dbp=$(aws secretsmanager get-secret-value --region eu-west-2 --secret-id db-creds | jq -r .SecretString | jq -r .password)
-
-    # drop -cl run in char mode 
-    #./oewizard -dbap manager -u soe -p soe -cl -cs //localhost/plorcl -ts SOE -drop
-    # 
-    #
-    # get db hostname
-    # aws rds 
-    #
-    # create
-    cd ~/swingbench/bin
-    ./oewizard  -dba admin -dbap $dbp -u soe -p soe -cl -cs //$db1/orcl -ts SOE -scale 32 -create -tc 8 -v 
+    ./oewizard  -dba admin -dbap $dbp -u soe -p soe -cl -cs //$db2/orcl -ts SOE -scale 32 -create -tc 8 -v 
     if [ $? -ne 0 ]; then
         echo "ERROR:  oewizard non zero exit code "
     fi
 fi
 
-db1=$(aws rds describe-db-instances --region eu-west-2 --db-instance-identifier dwp-demo-single --query "DBInstances[].Endpoint.Address" | jq -r .[])
-if [ $db1 != "" ];then 
-    nmap $db1 -Pn -p 1521 | grep open
+
+if [ $db3 != "" ];then 
+    nmap $db3 -Pn -p 1521 | grep open
     echo "swingbench oewizard"
     date
 
-    dbp=$(aws secretsmanager get-secret-value --region eu-west-2 --secret-id db-creds | jq -r .SecretString | jq -r .password)
-
-    # drop -cl run in char mode 
-    #./oewizard -dbap manager -u soe -p soe -cl -cs //localhost/plorcl -ts SOE -drop
-    # 
-    #
-    # get db hostname
-    # aws rds 
-    #
-    # create
     cd ~/swingbench/bin
-    ./oewizard  -dba admin -dbap $dbp -u soe -p soe -cl -cs //$db1/orcl -ts SOE -scale 32 -create -tc 8 -v 
+    ./oewizard  -dba admin -dbap $dbp -u soe -p soe -cl -cs //$db3/orcl -ts SOE -scale 32 -create -tc 8 -v 
     if [ $? -ne 0 ]; then
         echo "ERROR:  oewizard non zero exit code "
     fi
 fi
-
 
 # 1 thread 2m 19 - 2 threads 3m 52 - 4 threads 2m 42
 echo "Finished oewizard"
