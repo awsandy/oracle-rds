09f5554e5b062ac9b9ba1f641a4a285dad67b5d1
awsandy <awsandy@amazon.co.uk>
2021-05-04 09:56:50 +0100

    up

--- swingbench/char-single.sh
+++ swingbench/char-single.sh
@@ -1,1 +1,3 @@
-bin/charbench -c ../configs/myconfig-single.xml -a -v users,tpm,tps,resp,dml,trem -rt 0:10
+bin/charbench -c ../configs/myconfig-single.xml -a -v users,tpm,tps,resp,dml,trem -rt 0:10 -r rsingle.xml
+grep AverageTr bin/rsingle.xml
+grep TotalC bin/rsingle.xml
