2fe74b4613ceb15d9453f5daa4d1aa4e838b90e1
awsandy <awsandy@amazon.co.uk>
2022-02-10 10:59:10 +0000

    up

--- DAS-kinesis/das-script.py
+++ DAS-kinesis/das-script.py
@@ -71,6 +71,7 @@ def main():
                 rgs=str(decrypt_decompress(payload_decoded, data_key_decrypt_result['Plaintext']))
                 rgs = rgs[2:]
                 rgs = rgs[:-1]
+                print("rgs=",rgs)
                 print(json.dumps(rgs, indent=4, separators=(',', ': ')))
             if 'NextShardIterator' in response:
                 next_shard_iters.append(response['NextShardIterator'])
