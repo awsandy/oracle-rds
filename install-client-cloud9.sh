f673ae112edc594789c084a045c1261c54c9615c
awsandy <awsandy@amazon.co.uk>
2022-02-17 21:28:59 +0000

    up

--- install-client-cloud9.sh
+++ install-client-cloud9.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
 set +x
+cd ~/environment
+sudo yum install -q -y wget smartmontools deltarpm jq nmap
+sudo yum reinstall python3-pip -y
+# Increase the disk size to 32GB
 # ------  resize OS disk -----------
 
 # Specify the desired volume size in GiB as a command-line argument. If not specified, default to 20 GiB.
@@ -27,8 +32,8 @@ while [ \
 sleep 1
 done
 
-if [ $(readlink -f /dev/xvda) = "/dev/xvda" ]
-then
+$(readlink -f /dev/xvda) 2> /dev/null
+if [[ $? -eq 0 ]];then
   # Rewrite the partition table so that the partition takes up all the space that it can.
   sudo growpart /dev/xvda 1
  
@@ -44,21 +49,51 @@ else
   sudo xfs_growfs /dev/nvme0n1p1 #(Amazon Linux 2)
 fi
 
+echo "Install aws cli v2"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -qq awscliv2.zip
+sudo ./aws/install
+rm -f awscliv2.zip
+
+date
+echo "======= X11 mate desktop install ..."
+sudo amazon-linux-extras install -y mate-desktop1.x > /dev/null
+sudo echo "PREFERRED=/usr/bin/mate-session" > /etc/sysconfig/desktop
+
+sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+sudo systemctl set-default graphical.target
+echo "======= xrdp"
+sudo yum -y install xrdp tigervnc-server -q 2> /dev/null
+#yum -y install xrdp
+sudo systemctl start xrdp
+sudo systemctl enable xrdp
+sudo netstat -antup | grep xrdp
+
+echo "X11 stuff again ......"
+sudo amazon-linux-extras install -y mate-desktop1.x
+echo "PREFERRED=/usr/bin/mate-session" > desktop
+sudo cp desktop /etc/sysconfig/desktop
+echo "install chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -q
+sudo yum install google-chrome-stable_current_*.rpm -y -q 
+echo "DB Beaver"
+wget https://dbeaver.io/files/dbeaver-ce-latest-stable.x86_64.rpm -q
+sudo yum install dbeaver-ce-latest-stable.x86_64.rpm  -y -q 2> /dev/null
+#echo "Lens"
+#wget https://api.k8slens.dev/binaries/Lens-5.3.4-latest.20220120.1.x86_64.rpm -q
+#sudo yum  install Lens-5.3.4-latest.20220120.1.x86_64.rpm  -y -q 2> /dev/null
+echo "VS Code"
+wget -O vscode.rpm https://go.microsoft.com/fwlink/?LinkID=760867 -q
+sudo yum install vscode.rpm  -y -q 2> /dev/null
+sudo cat /etc/sysconfig/desktop
+
+
 sudo yum install -q -y wget smartmontools deltarpm jq nmap
-#echo "SSM agent"
-#wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-#yum install -y amazon-ssm-agent.rpm
-#systemctl enable amazon-ssm-agent
-#systemctl start amazon-ssm-agent
-#systemctl status amazon-ssm-agent
+
 echo "======= yum compat"
 sudo yum install -y compat-libcap1.x86_64 gcc.x86_64 gcc-c++.x86_64 glibc.i686 glibc.x86_64 glibc-devel.i686 glibc-devel.x86_64 ksh compat-libstdc++-33 libgcc.i686 libstdc++.i686 libstdc++-devel.i686 libstdc++-devel.x86_64 libXi.i686 libXi.x86_64 libXtst.i686 libXtst.x86_64
-#firewall-cmd --add-port=3389/tcp --permanent
-#systemctl stop firewalld
-#systemctl disable firewalld
-#sed -i 's/SELINUX=enforcing/SELINUX=permissive/' /etc/selinux/config
-#setenforce Permissive
-#hostnamectl set-hostname client.local
+
 sudo groupadd oinstall
 sudo groupadd dba
 sudo useradd -g oinstall -G dba oracle
@@ -83,42 +118,21 @@ sudo echo "oracle soft nofile 1024" >> /etc/security/limits.conf
 sudo echo "oracle hard nofile 65536" >> /etc/security/limits.conf
 sudo echo "oracle soft stack 10240" >> /etc/security/limits.conf
 sudo echo "oracle hard stack 32768" >> /etc/security/limits.conf
-echo "aws cli"
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip -qq awscliv2.zip
-./aws/install
-
-date
-echo "======= X11 mate "
-sudo which  amazon-linux-extras
-sudo amazon-linux-extras install -y mate-desktop1.x
-sudo echo "PREFERRED=/usr/bin/mate-session" > /etc/sysconfig/desktop
-
-sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-
-sudo systemctl set-default graphical.target
-echo "======= xrdp"
-sudo yum -y install xrdp tigervnc-server
-#yum -y install xrdp
-sudo systemctl start xrdp
-sudo systemctl enable xrdp
-sudo netstat -antup | grep xrdp
-date
 
 echo "======= Corretto Java"
 
 sudo rpm --import https://yum.corretto.aws/corretto.key 
 sudo curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
-sudo yum install -y java-15-amazon-corretto-devel
+sudo yum install -y java-15-amazon-corretto-devel -q
 
 echo "======= Oracle software get to /software"
 cd /home/ec2-user/environment
 mkdir software
 cd software
-echo "======= swingbench get to /software"
+echo "======= swingbench get to ~/environment/software"
 wget -q https://github.com/domgiles/swingbench-public/releases/download/production/swingbenchlatest.zip
-echo "======= SCT get to /software"
-wget -q https://s3.amazonaws.com/publicsctdownload/Fedora/aws-schema-conversion-tool-1.0.latest.zip
+#echo "======= SCT get to /software"
+#wget -q https://s3.amazonaws.com/publicsctdownload/Fedora/aws-schema-conversion-tool-1.0.latest.zip
 #wget -q https://www.oracle.com/database/technologies/jdbc-ucp-122-downloads.html
 echo "======= get oracle inst client & sqlplus"
 #aws s3 cp s3://oracle-swingbench/clients/oracle-instantclient-basic-21.1.0.0.0-1.x86_64.rpm oracle-instantclient-basic-21.1.0.0.0-1.x86_64.rpm  --quiet
@@ -127,20 +141,14 @@ aws s3 cp s3://oracle-swingbench/clients/oracle-instantclient19.14-sqlplus-19.14
 echo "======= install oracle inst basic client"
 sudo  yum install -y oracle-instantclient-basic-21.1.0.0.0-1.x86_64.rpm
 sudo  yum install -y oracle-instantclient19.14-sqlplus-19.14.0.0.0-1.x86_64.rpm
-sudo  chown oracle.oinstall *
-#chmod -R 755 /software/*.sh
-sudo ls software
-#echo "======= Oracle clinstall-1 - swingbench" 
-#sudo -u oracle -- sh -c "/software/oracle-rds/clinstall-1.sh"
-echo "X11 stuff again ......"cd
-sudo amazon-linux-extras install -y mate-desktop1.x
-sudo echo "PREFERRED=/usr/bin/mate-session" > /etc/sysconfig/desktop
+unzip -qq swingbenchlatest.zip
+sudo chown -R oracle.oinstall *
+sudo chmod -R 755 /software/*.sh
+
 #rsp=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag:Oracle,Values=19c" | jq -r .Reservations[].Instances[].PrivateIpAddress)
 #echo "Server:  $rsp"
 #echo "$rsp oraclelinux" >> /etc/hosts
-echo "install chrome"
-sudo curl https://intoli.com/install-google-chrome.sh | bash
-echo "surf for lens"
-date
-# /usr/bin/google-chrome-stable
+
+
 echo "look in /usr/share/applications"
+echo "Instance ID = $INSTANCE_ID"
