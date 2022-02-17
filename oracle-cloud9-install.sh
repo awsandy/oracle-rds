debb8123c26d053f89f2b6c60ccb756a45b6b494
awsandy <awsandy@amazon.co.uk>
2022-02-17 22:38:49 +0000

    update

--- oracle-cloud9-install.sh
+++ oracle-cloud9-install.sh
@@ -11,7 +11,7 @@ echo "======= swingbench get to /home/oracle/software"
 wget -q https://github.com/domgiles/swingbench-public/releases/download/production/swingbenchlatest.zip
 unzip -qq swingbenchlatest.zip
 chown -R oracle.oinstall *
-chmod -R 755 *.sh
+#chmod -R 755 *.sh
 
 #rsp=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag:Oracle,Values=19c" | jq -r .Reservations[].Instances[].PrivateIpAddress)
 #echo "Server:  $rsp"
